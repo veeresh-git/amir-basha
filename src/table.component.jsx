0d0740c491d8f15faf62bc3c52ecf6ba0dd832f1
veeresh <veereshv537@gmail.com>
2021-10-22 20:08:50 +0530

    done

---
+++ src/table.component.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+
+class TableComponent extends React.Component {
+  render() {
+    return (
+      <div className="box_container">
+        <div className="box_container_inner">
+          <div className="top_container">
+            <div className="inner_box2">
+              <textarea cols="40" rows="2" className="input_field"></textarea>
+            </div>
+            <div className="inner_box2">
+              <textarea cols="40" rows="2" className="input_field"></textarea>
+            </div>
+            <div className="inner_box2">
+              <textarea cols="40" rows="2" className="input_field"></textarea>
+            </div>
+            <div className="inner_box2">
+              <textarea cols="40" rows="2" className="input_field"></textarea>
+            </div>
+          </div>
+          <div className="middle1_container">
+            <div className="inner_box2">
+              <textarea cols="40" rows="2" className="input_field"></textarea>
+            </div>
+            <div
+              className="inner_box2"
+              style={{ border: "none", width: "100px" }}
+            >
+              <textarea cols="40" rows="2" className="input_field"></textarea>
+            </div>
+            <div className="inner_box2">
+              <textarea cols="40" rows="2" className="input_field"></textarea>
+            </div>
+          </div>
+          <div className="middle1_container">
+            <div className="inner_box2">
+              <textarea cols="40" rows="2" className="input_field"></textarea>
+            </div>
+            <div className="inner_box2">
+              <textarea cols="40" rows="2" className="input_field"></textarea>
+            </div>
+          </div>
+          <div className="top_container">
+            <div className="inner_box2">
+              <textarea cols="40" rows="2" className="input_field"></textarea>
+            </div>
+            <div className="inner_box2">
+              <textarea cols="40" rows="2" className="input_field"></textarea>
+            </div>
+            <div className="inner_box2">
+              <textarea cols="40" rows="2" className="input_field"></textarea>
+            </div>
+            <div className="inner_box2">
+              <textarea cols="40" rows="2" className="input_field"></textarea>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default TableComponent;
